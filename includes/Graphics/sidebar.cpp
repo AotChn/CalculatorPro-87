6932d2c7e6554feefa25b263ef57e317fae0b43e
agentaot <kiattiphumaot@gmail.com>
2022-12-10 01:00:55 -0800

    added bottom bar

--- includes/Graphics/sidebar.cpp
+++ includes/Graphics/sidebar.cpp
@@ -5,13 +5,16 @@ Sidebar::Sidebar()
 
 Sidebar::Sidebar(float left, float width) : _left(left), _width(width){
     cout << "Sidebar CTOR: TOP" << endl;
-    items.reserve(50);
 
     // set up the sidebar rectangle:
-    rect.setFillColor(sf::Color(10,10,10,255)); //(192,192,192)); //silver
+    rect.setFillColor(sf::Color(50,50,50,255)); //(192,192,192)); //silver
     rect.setPosition(sf::Vector2f(left,0));
     rect.setSize(sf::Vector2f(width, SCREEN_HEIGHT));
     cout << "Sidebar CTOR: about to load font." << endl;
+
+    bottomBar.setFillColor(sf::Color(89,157,255,255));
+    bottomBar.setPosition(sf::Vector2f(0,SCREEN_HEIGHT-45));
+    bottomBar.setSize(sf::Vector2f(SCREEN_WIDTH,45));
 
     ////- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     // the font file must be in the "working directory"
@@ -43,6 +46,18 @@ Sidebar::Sidebar(float left, float width) : _left(left), _width(width){
     sb_text.setStyle(sf::Text::Bold);
     sb_text.setFillColor(sf::Color::Yellow);
 
+    current_eq.setFont(font);
+    current_eq.setCharacterSize(40);
+    current_eq.setStyle(sf::Text::Bold);
+    current_eq.setFillColor(sf::Color::Black);
+    current_eq.setPosition(sf::Vector2f(30, SCREEN_HEIGHT-50));
+
+    domain.setFont(font);
+    domain.setCharacterSize(25);
+    domain.setStyle(sf::Text::Bold);
+    domain.setFillColor(sf::Color::Black);
+    domain.setPosition(sf::Vector2f(400, SCREEN_HEIGHT-(bottomBar.getSize().y/2)-15));
+
     ////this is how you would position text on screen:
     // sb_text.setPosition(sf::Vector2f(10, SCREEN_HEIGHT-sb_text.getLocalBounds().height-5));
 
@@ -57,29 +72,41 @@ Sidebar::Sidebar(float left, float width) : _left(left), _width(width){
 void Sidebar::draw(sf::RenderWindow &window){
     const double VERTICAL_LINE_SPACING = 5.0;
     const double LEFT_MARGIN = 10.0;
-
     window.draw(rect);
-    float height = 10;
+    window.draw(bottomBar);
+    window.draw(current_eq);
+    window.draw(domain);
 
-    for (vector<string>::iterator it = items.begin();
-         it != items.end(); it++){
-        bool blank = false;
-        if (it->length() == 0){
-            // empty rows must be taken into account (getLocalBounds())
-            //     but not drawn
-            blank = true;
-            sb_text.setString("BLANK");
-        }
-        else{
-            sb_text.setString(it->c_str());
-        }
-        sb_text.setPosition(sf::Vector2f(_left + LEFT_MARGIN, height));
-        height += sb_text.getLocalBounds().height + VERTICAL_LINE_SPACING;
-        if (!blank)
-            window.draw(sb_text);
-    }
+    
 }
 
 string &Sidebar::operator[](int index){
     return items[index];
+}
+
+void Sidebar::set_bottom_Bar_info(Graph_info* info){
+    current_eq.setString(info->Eq);
+    std::string domain_string = "Domain : (";
+    domain_string += std::to_string(info->Screen_domain.x);
+    domain_string += " , ";
+    domain_string += std::to_string(info->Screen_domain.y);
+    domain_string += ") | Range : (";
+    domain_string += std::to_string(info->Screen_range.x);
+    domain_string += " , ";
+    domain_string += std::to_string(info->Screen_range.y);
+    domain_string += ")";
+    domain.setString(domain_string);
+}
+
+sf::RectangleShape Sidebar::create_button(int i,int j){
+    sf::RectangleShape box1;
+    if(i==1){
+        box1.setFillColor(sf::Color(66,68,77,255));
+    }
+    else{
+         box1.setFillColor(sf::Color(66,68,77,100));
+    }
+    box1.setPosition(sf::Vector2f(_left,(SCREEN_HEIGHT/10*j)));
+    box1.setSize(sf::Vector2f(_width,SCREEN_HEIGHT/10));
+    return box1;
 }
--- includes/Graphics/sidebar.h
+++ includes/Graphics/sidebar.h
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "../data_pkg/graph_info.h"
 using namespace std;
 
 
@@ -24,15 +25,19 @@ public:
     Sidebar(float left, float width);
     void draw(sf::RenderWindow& window);
     string& operator [](int index);
+    void set_bottom_Bar_info(Graph_info* info);
+    void history_buttons();
+    sf::RectangleShape create_button(int i,int j);
 private:
     sf::RectangleShape rect;            //sidebar rectangle
     vector<string> items;               //strings to place on the sidebar
     sf::Font font;                      //used to draw text
     sf::Text sb_text;                   //used to draw strings on the window object
-
+    sf::RectangleShape bottomBar;
     float _left;
     float _width;
-
+    sf::Text current_eq;
+    sf::Text domain;
 };
 
 #endif // SIDEBAR_H
