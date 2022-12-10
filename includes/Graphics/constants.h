839456b105537c5cfe46a658674b0e171b3e8094
agentaot <kiattiphumaot@gmail.com>
2022-12-10 01:00:41 -0800

    added screen ratios

--- includes/Graphics/constants.h
+++ includes/Graphics/constants.h
@@ -1,10 +1,13 @@
 #ifndef GRAPH_CONSTANTS_H
 #define GRAPH_CONSTANTS_H
 const float SCREEN_WIDTH = 2500;
-const float SCREEN_HEIGHT = 2000;
-const float WORK_PANEL = SCREEN_WIDTH*4/5;
-const float SIDE_BAR = SCREEN_WIDTH*1/5;
+const float SCREEN_HEIGHT = SCREEN_WIDTH*2/3;
+const float WORK_PANEL = SCREEN_WIDTH*2/3;
+const float SIDE_BAR = SCREEN_WIDTH*1/3;
 const float TOTAL_PTS = 750;
+
+const float BUTTON_SIZE = SCREEN_HEIGHT/10;
+
 
 const int SB_MOUSE_POSITION = 0;
 const int SB_MOUSE_CLICKED = SB_MOUSE_POSITION + 1;
