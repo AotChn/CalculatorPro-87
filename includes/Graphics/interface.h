886f65a08737ee8c74d2469aa13557622c13c352
agentaot <kiattiphumaot@gmail.com>
2022-12-11 00:12:49 -0800

    changed labels

--- includes/Graphics/interface.h
+++ includes/Graphics/interface.h
@@ -48,13 +48,24 @@ void set_axis_labels(sf::RenderWindow& window){
     else if(posY>SCREEN_HEIGHT-80){
         posY = SCREEN_HEIGHT-80;
     }
-    label.setString(std::to_string(info->Screen_domain.x));
-    label.setPosition(sf::Vector2f(0,posY));
-    window.draw(label); //min x
+    if(info->scale.y>=-1){
+        label.setString(std::to_string((int)info->Screen_domain.x));
+        label.setPosition(sf::Vector2f(0,posY));
+        window.draw(label); //min x
 
-    label.setString(std::to_string(info->Screen_domain.y));
-    label.setPosition(sf::Vector2f(info->window_size.x-140,posY));
-    window.draw(label); //max x
+        label.setString(std::to_string((int)info->Screen_domain.y));
+        label.setPosition(sf::Vector2f(info->window_size.x-140,posY));
+        window.draw(label); //max x
+    }
+    else{
+        label.setString(std::to_string(info->Screen_domain.x));
+        label.setPosition(sf::Vector2f(0,posY));
+        window.draw(label); //min x
+
+        label.setString(std::to_string(info->Screen_domain.y));
+        label.setPosition(sf::Vector2f(info->window_size.x-140,posY));
+        window.draw(label); //max x
+    }
 
     posX = (info->intervals/2*SDI)+info->x_offset.x+info->scale.y;
     if(posX<0){
@@ -64,13 +75,24 @@ void set_axis_labels(sf::RenderWindow& window){
         posX = WORK_PANEL-140;
     }
 
-    label.setString(std::to_string(info->Screen_range.y));
-    label.setPosition(sf::Vector2f(posX,0));
-    window.draw(label); //min y
-    
-    label.setString(std::to_string(info->Screen_range.x));
-    label.setPosition(sf::Vector2f(posX,info->window_size.y-120));
-    window.draw(label); //max y
+    if(info->scale.y>=-1){
+        label.setString(std::to_string((int)info->Screen_range.y));
+        label.setPosition(sf::Vector2f(posX,0));
+        window.draw(label); //min y
+        
+        label.setString(std::to_string((int)info->Screen_range.x));
+        label.setPosition(sf::Vector2f(posX,info->window_size.y-120));
+        window.draw(label); //max y
+    }
+    else{
+        label.setString(std::to_string(info->Screen_range.y));
+        label.setPosition(sf::Vector2f(posX,0));
+        window.draw(label); //min y
+        
+        label.setString(std::to_string(info->Screen_range.x));
+        label.setPosition(sf::Vector2f(posX,info->window_size.y-120));
+        window.draw(label); //max y
+    }
 }
 
 private:
