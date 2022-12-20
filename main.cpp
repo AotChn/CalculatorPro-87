a123fddba2c96049c6fde2c99c031684c822bf64
Aot Chanthorn <37425542+AotChn@users.noreply.github.com>
2022-12-20 01:56:46 -0800

    check

--- main.cpp
+++ main.cpp
@@ -1,37 +1,13 @@
 #include <iostream>
-#include <iomanip>
-#include <cmath>
-#include <math.h>
-#include <map>
-
-//TEST FILES ------------------------------------------------------>[]
-#include "includes/rpn/rpn.h"
-#include "includes/node/node.h"
-#include "includes/token/token.h"
-#include "includes/token/operator.h"
-#include "includes/token/integer.h"
-#include "includes/token/function.h"
-#include "includes/token/leftparen.h"
-#include "includes/token/rightparen.h"
-#include "includes/token/function.h"
-#include "includes/queue/MyQueue.h"
-#include "includes/token/constants.h"
 #include "includes/Graphics/animate.h"
-#include "includes/Graphics/system.h"
-#include "includes/Graphics/sidebar.h"
-#include "includes/tokenizer/tokenizer.h"
-#include "includes/shunting_yard/shunting_yard.h"
-#include "includes/linked_list_functions/linked_list_functions.h"
 
-//++++++++++ ------------------------------------------------------>[]
+//++++++++++
 
-int main(int argv, char** argc) {
-    std::cout << "\n\n"<< endl;
+int main() {
 
    Animate calculator;
    calculator.run();
-
-    std::cout << "\n\n\n=====================" << endl;
-    return 0;
+   
+   return 0;
 }
 
